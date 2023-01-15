fcb3146720bbaf06a3aa5edc03bf6cc7d7db0862
Nurbek Ismoilov <nuronbeck@gmail.com>
2023-01-16 02:56:47 +0500

    Autopublish trigger with express server

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
 const { Telegraf } = require("telegraf");
-const schedule = require('node-schedule');
+// const schedule = require('node-schedule');
 const axios = require('axios');
+
+// Express server
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Express server middlewares
+app.use(cors({ origin: '*' }));
 
 // Settings
 const botToken = process.env.BOT_TOKEN;
 const channel = `@${process.env.CHANNEL_NAME}`;
 
 // Schedule rule
-const scheduleRule = new schedule.RecurrenceRule();
-scheduleRule.hour = 10;
-scheduleRule.minute = 0;
-scheduleRule.tz = 'Asia/Tashkent';
+// const scheduleRule = new schedule.RecurrenceRule();
+// scheduleRule.hour = 2;
+// scheduleRule.minute = 41;
+// scheduleRule.tz = 'Asia/Tashkent';
 
 // Telegram bot instance
 const bot = new Telegraf(botToken);
 
-const job = schedule.scheduleJob(scheduleRule, async function(){
-  console.log("Schedule job has been started...");
+async function autopostTelegramChannel () {
+  console.log('Started autoposting...');
 
   const { data: ipakYoliBankRate = {} } = await axios.get('https://ipak-yoli-bank-exchange-rates.onrender.com/rate');
   
@@ -29,9 +39,24 @@ const job = schedule.scheduleJob(scheduleRule, async function(){
 
   console.log(ipakYoliBankRateMarkup);
 
-  bot.telegram.sendMessage(
+  return bot.telegram.sendMessage(
     channel, ipakYoliBankRateMarkup, { parse_mode: 'HTML'}
   );
+}
 
-  console.log("Schedule job is finished!");
+// const job = schedule.scheduleJob(scheduleRule, async function(){
+//   console.log("Schedule job has been started...");
+//   await autopostTelegramChannel();
+//   console.log("Schedule job is finished!");
+// });
+
+
+app.post('/publish-post', (req, res) => {
+  autopostTelegramChannel();
+  
+  res.status(200).send({ message: 'Ok' });
 });
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+})
